b03aced3de6408bcb4b34e2111f4e7b856611686
SupriseSelinda <selindasurprise22@gmail.com>
2024-05-07 15:14:44 +0200

    Class Commit

---
+++ Selinda220792844.java
@@ -0,0 +1,10 @@
+public class Selinda220792844 {
+
+    public static void main(String[] args) {
+         
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int sum = firstNumber + secondNumber;
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+    }
+}
